d1da613c3efece2ac330d913b94926ddf6307091
maximilian-engelhardt <maximilian.engelhardt@lhind.dlh.de>
2020-11-08 15:15:54 +0100

    added repo structure and endpoints module

---
+++ src/api/endpoints.py
@@ -0,0 +1,66 @@
+# imports
+import numpy as np
+from typing import Dict
+from fastapi import APIRouter, HTTPException
+from starlette.requests import Request
+
+# local absolute imports
+from src.utils.utils import category_mapping
+from run import run_training
+from src.api.model import NewsCategory, NewsTitle, NewsClassifier
+
+
+def get_prediction(request: Request, payload: NewsTitle) -> NewsCategory:
+    """Predicts the category of a given news title.
+
+    Args:
+        request (Request): The starlette request object.
+        payload (NewsTitle): The json payload of the request with
+            fields defined in the pydantic ```NewsTitle```object.
+
+    Raises:
+        HTTPException: An exception if no model is found.
+
+    Returns:
+        NewsCategory: The json response with fields defined in
+            the pydantic ```NewsCategory```object.
+    """
+
+    title = payload.title
+
+    try:
+        model = request.app.state.model
+    except:
+        raise HTTPException(
+            status_code=400,
+            detail="No model found."
+        )
+
+    prediction = model.predict([title])
+    label = prediction[0]
+    category = category_mapping()[label]
+
+    return NewsCategory(
+        title=title,
+        label=label,
+        category=category
+    )
+
+def train_model(request: Request, pipeline_config: str) -> Dict:
+    """Optional endpoint for training and deploying a new model.
+
+    Returns:
+        response (Dict): jsonified dictionary with information
+            about the training run and the new model id.
+    """
+
+    run, final_params, test_results = run_training(pipeline_config)
+    new_model = NewsClassifier(run.info.run_id)
+    request.app.state.model = new_model
+
+    return {
+        "new model id": run.info.run_id,
+        "final hyperparameters": final_params,
+        "evaluation results": test_results,
+        "run information": run.info,
+    }
